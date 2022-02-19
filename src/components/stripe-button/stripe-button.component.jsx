3e64f19fc50d567209454e34fd2f358afc3d9ff6
Luis Telemaco <ltelemaco14@gmail.com>
2022-02-19 19:40:23 -0400

    Updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KV1VqIz4EqdTTwbKHRevhN06OVM3Gp3Dlq4bg3ZSLMTd8rQTYxcYRld3FlU4bO7bMARvEbwdigyf1Vd1uoqiCc000Y4JR0OZ9";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddressImage="htps://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-
 import {
   selectCartTotal,
   selectCartItems,
 } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./Sass/checkout.styles.scss";
 
@@ -36,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span> Total: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/23 - CVV: 123{" "}
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
